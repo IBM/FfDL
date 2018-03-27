efb64d5a40b33093254c8b3f8d7c713ab75f512e
Tommy Li <Tommy.chaoping.li@ibm.com>
2018-03-27 12:34:09 -0700

    Update train.sh path to match with DLaaS.

--- lcm/service/lcm/container_helper_extensions.go
+++ lcm/service/lcm/container_helper_extensions.go
@@ -35,7 +35,7 @@ func extendLearnerContainer(learner *v1core.Container, req *service.JobDeploymen
 		// TODO!
 	}
 
-	extCmd := "export PATH=/:$PATH; cp " + learnerEntrypointFilesPath + "/*.sh /; chmod +x /*.sh; "
+	extCmd := "export PATH=/usr/local/bin/:$PATH; cp " + learnerEntrypointFilesPath + "/*.sh /usr/local/bin/; chmod +x /usr/local/bin/*.sh;"
 	extMount := v1core.VolumeMount{
 		Name:      learnerEntrypointFilesVolume,
 		MountPath: learnerEntrypointFilesPath,
