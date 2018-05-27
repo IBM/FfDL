3093f0d7cdce6d79b6348d22997682b0348ab05e
Falk Pollok <falk.pollok@ibm.com>
2018-05-27 10:50:34 -0400

    Minor DIND script update

--- bin/dind_scripts/compile_s3fs.sh
+++ bin/dind_scripts/compile_s3fs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get update -qq
-sudo apt-get install -y build-essential git-core libcurl4-openssl-dev libxml2-dev pkg-config autotools-dev automake libfuse-dev fuseliblibssl-dev
+cd ~
+sudo apt-get install -y build-essential git-core libcurl4-openssl-dev libxml2-dev pkg-config autotools-dev automake libfuse-dev libssl-dev
 git clone https://github.com/s3fs-fuse/s3fs-fuse.git
 cd s3fs-fuse
 ./autogen.sh
