119c541a309ff31fa050d62815166e05aacee6fa
Christian Kadner <ckadner@us.ibm.com>
2018-10-15 13:54:01 -0700

    Configurable model file name in seldon-deployment for fashion demo
    With the model file name being configurable it is possible to re-use
    the seldon-deployment part of the fashion demo for other models.

--- demos/fashion-mnist-training/seldon-deployment/MyModel.py
+++ demos/fashion-mnist-training/seldon-deployment/MyModel.py
@@ -13,6 +13,7 @@ class MyModel(object):
         bucket_name = os.environ.get("BUCKET_NAME")
         bucket_key = os.environ.get("BUCKET_KEY")
         bucket_secret = os.environ.get("BUCKET_SECRET")
+        model_file_name = os.environ.get("MODEL_FILE_NAME", "model.h5")
         print("Training id:{} endpoint URL:{} key:{} secret:{} bucket name:{}".format(training_id,endpoint_url,bucket_key,bucket_secret,bucket_name))
 
         # Define S3 resource and download the model file
@@ -23,10 +24,11 @@ class MyModel(object):
             aws_secret_access_key=bucket_secret,
         )
 
-        KEY = training_id + '/model.h5' # replace with your object key
+        KEY = training_id + '/' + model_file_name
+        model_path = 'model.h5'
 
         try:
-            client.Bucket(bucket_name).download_file(KEY, 'model.h5')
+            client.Bucket(bucket_name).download_file(KEY, model_path)
         except botocore.exceptions.ClientError as e:
             if e.response['Error']['Code'] == "404":
                 print("The object does not exist.")
@@ -34,7 +36,6 @@ class MyModel(object):
                 raise
 
         # Replace with path of trained model
-        model_path = 'model.h5'
         self.model = load_model(model_path)
 
     def predict(self,X,features_names):
